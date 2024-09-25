5c70e72939672bc1e68c28f29c3768d043d492c4
Anarxist <merkaru1@gmail.com>
2024-09-25 22:02:48 +0300

    test3

--- MLCMatrix.h
+++ MLCMatrix.h
@@ -1,7 +1,14 @@
 #pragma once
+#include <iostream>
+#include <vector>
 class MLCMatrix {
-
-
+private:
+	std::vector<std::vector<double>> data;
+	int string=0;
+	int column=0;
+	MLCMatrix(int rows, int cols) {
+		data.resize(rows, std::vector<double>(cols, 0.0));
+	}
 
 
 };
